37349ffe907d10f59fb678ebc9a18041891a43fe
jammin2shirts <teuchertb@gmail.com>
2022-05-22 21:03:57 -0400

    Got the nav looking good in desktop mode

--- wedding-barn/src/app/component/header/header.component.ts
+++ wedding-barn/src/app/component/header/header.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  left_navs = [{text:"About", link:"abc"},{text:"Contact", link:"abc"},
+  {text:"FAQ", link:"def"},]
+  right_navs =[{text:"Pricing", link:"123"},
+  {text:"Calendar", link:"456"},{text:"Gallery", link:"abc"},]
+
+
   constructor() { }
 
   ngOnInit(): void {
