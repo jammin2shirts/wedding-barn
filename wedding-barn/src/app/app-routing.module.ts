9db6b3e4a884b67ae982d7dadce5b7a78dcd4332
jammin2shirts <teuchertb@gmail.com>
2022-07-09 20:40:56 -0400

    Made updates to landing page

--- wedding-barn/src/app/app-routing.module.ts
+++ wedding-barn/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './component/landing-page/landing-page.component';
 
 
 const routes: Routes = [
+  { path: '**', component: LandingPageComponent },
 ];
 
 @NgModule({
--- wedding-barn/src/app/app.module.ts
+++ wedding-barn/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { LandingPageComponent } from './component/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
