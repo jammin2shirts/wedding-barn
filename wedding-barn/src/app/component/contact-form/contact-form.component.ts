dbce904aa6cfefc723581f34f112698a96e4b7ee
jammin2shirts <teuchertb@gmail.com>
2022-10-26 08:24:03 -0400

    Updated footer and mobile menu

--- wedding-barn/src/app/component/contact-form/contact-form.component.ts
+++ wedding-barn/src/app/component/contact-form/contact-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { ScriptServiceService } from 'src/app/service/script-service.service';
 
 declare var $: any;
 @Component({
@@ -6,10 +7,27 @@ declare var $: any;
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
 })
-export class ContactFormComponent implements OnInit {
+export class ContactFormComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private renderer:Renderer2, private scriptService:ScriptServiceService) { }
+
+  ngOnDestroy(): void {
+  }
+
   ngOnInit(): void {
+    // const scriptElement = this.scriptService.loadJsScript(this.renderer, 'assets/js/honeybook-form.js');
+    console.log('loaded')
+    // var elements = document.getElementsByClassName('hb-p-633da926aa7d8e0009eb7aad-1');//.item.contentWindow.location.reload();
+    // // document.getElementById('id').src += '';
+    // console.log(<HTMLElement>elements[0]);
+    // (<HTMLElement>elements[0]).src +='';
+
+    // let myScript = document.createElement("script");
+    // myScript.type = 'text/javascript';
+    // myScript.async = true;
+    // myScript.src='assets/js/honeybook-form.js';
+    // document.body.appendChild(myScript)
+    
   }
  
   
